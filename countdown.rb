7be5334f6639506583048ffa0ef06be841dd931d
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-06-22 14:46:25 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,21 @@
 #write your code here
-
-def countdown
-  while number < 10
+def countdown(number)
+  while number > 0
+   sleep 1
     puts "#{number} SECOND(S)!"
-      x -= 1
-    x == 0
-      puts "HAPPY NEW YEAR!"
+    number -= 1
+  end
+  return "HAPPY NEW YEAR!"
 end
+
+countdown(10)
+
+def countdown_with_sleep(number)
+  while number > 0
+   sleep 1
+    puts "#{number} SECOND(S)!"
+    number -= 1
+  end
+  return "HAPPY NEW YEAR!"
 end
+
